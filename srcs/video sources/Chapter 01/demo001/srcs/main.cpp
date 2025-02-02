e6da0d81b2c62d9885e027c277c32ea94959ce9f
Acreate <572646005@qq.com>
2025-02-02 18:45:20 +0800

    迁移测试项目到cmakefunction

--- srcs/video sources/Chapter 01/demo001/srcs/main.cpp
+++ srcs/video sources/Chapter 01/demo001/srcs/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <tools/io.h>
 #include <tools/path.h>
-#include "../auto_generate_files/macro/cmake_to_c_cpp_header_env.h"
+#include "../auto_generate_files/macro/cmake_include_to_c_cpp_header_env.h"
 DEF_CURRENT_RELATIVELY_PATH_STATIC_VALUE( __FILE__ );
 DEF_CURRENT_PROJECT_NAME( );
 #define BUFFER_OFFSET(x)  ((const void*) (x))
@@ -8,7 +8,10 @@ DEF_CURRENT_PROJECT_NAME( );
 using namespace std;
 
 #include "stdio.h"
-
+DEF_ALL_CMAKE_PROPERTIES_VALUE_STD_VECTOR( );
+DEF_ALL_CMAKE_PROPERTIES_VALUE_GET_VALID_PAIRS_STD_VECTOR( );
 int main( int argc, char **argv ) {
-
+	auto pairs = getValidCmakePropertyPairs( );
+	for( auto &pair : pairs )
+		std::cout << pair.first << ":" << pair.second << std::endl;
 }
